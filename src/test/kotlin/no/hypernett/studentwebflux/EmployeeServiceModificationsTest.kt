0f90d9d8434853fbfa37d1e25af4ee758fe9836b
Dan-Mikkel Hübenette <danmikkelhubenette@gmail.com>
2022-06-16 14:18:47 +0200

    Fått en test på plass, men den feiler

---
+++ src/test/kotlin/no/hypernett/studentwebflux/EmployeeServiceModificationsTest.kt
@@ -0,0 +1,25 @@
+package no.hypernett.studentwebflux
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest
+import org.springframework.context.annotation.Import
+import reactor.test.StepVerifier
+
+@DataR2dbcTest
+@Import(EmployeeService::class)
+class EmployeeServiceModificationsTest {
+
+    lateinit var service: EmployeeService
+    lateinit var repository: EmployeeRepository
+
+    @Test
+    fun `create returns the created Employee`() {
+        val createdEmployee = service.create("Karsten Beate Larsen", 1000.0, "Oslo")
+
+        StepVerifier
+            .create(createdEmployee)
+            .expectNextMatches { it.id != null }
+            .verifyComplete()
+    }
+
+}
